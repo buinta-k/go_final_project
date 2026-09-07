c2ad33ec76a4b765aa82576ecff76e9140e07b88
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-09-07 10:24:19 +0300

    Update settings.go

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzIjoiNTk5NDQ3MWFiYjAxMTEyYWZjYzE4MTU5ZjZjYzc0YjRmNTExYjk5ODA2ZGE1OWIzY2FmNWE5YzE3M2NhY2ZjNSJ9.h9uWqCmwvBiPszeQCAAU6QjRrBuk8LGbKmPYMCZN-_I`
